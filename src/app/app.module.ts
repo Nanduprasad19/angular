c4b913bd7825b3ada58994f78d3b6d350e9cfdad
unknown <nanduprasad99055@gmail.com>
2023-02-17 11:52:44 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ArtformsComponent } from './pages/artforms/artforms.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NavbarComponent } from './units/navbar/navbar.component';
+import { SinglepageComponent } from './pages/singlepage/singlepage.component';
+import { FooterComponent } from './units/footer/footer.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ArtformsComponent,
+    SignupComponent,
+    LoginComponent,
+    NavbarComponent,
+    SinglepageComponent,
+    FooterComponent,
+    ContactComponent,
+
   ],
   imports: [
     BrowserModule,
